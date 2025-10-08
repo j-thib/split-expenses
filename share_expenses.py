43b89435078aaeb7f67377f91758fd6caf3c7469
Jesse Thibodeau <96755740+j-thib@users.noreply.github.com>
2025-10-08 17:38:25 -0400

    make consistent terminology

--- share_expenses.py
+++ share_expenses.py
@@ -1,33 +1,33 @@
 import numpy as np
 
-def share_expenses(spending_history: dict):
+def share_expenses(spending_log: dict):
     """
     Takes a dictionary of expenses incurred {<name>: <amount>} and
     computes balances to be settled.
     """
-    total_spent = sum(spending_history.values())
-    total_per_person = np.round(total_spent / len(spending_history.keys()), 2)
+    total_spent = sum(spending_log.values())
+    total_per_person = np.round(total_spent / len(spending_log.keys()), 2)
 
-    debt_collectors = {}
-    indebted = {}
-    for name in spending_history.keys():
-        if spending_history[name] > total_per_person:
-            ar = np.round(spending_history[name] - total_per_person, 2)
-            debt_collectors[name] = ar
+    creditors = {}
+    debtors = {}
+    for name in spending_log.keys():
+        if spending_log[name] > total_per_person:
+            ar = np.round(spending_log[name] - total_per_person, 2)
+            creditors[name] = ar
             print(f"{name} is owed {ar}")
-        elif spending_history[name] < total_per_person:
-            ap = np.round(np.abs(spending_history[name] - total_per_person), 2)
-            indebted[name] = ap
+        elif spending_log[name] < total_per_person:
+            ap = np.round(np.abs(spending_log[name] - total_per_person), 2)
+            debtors[name] = ap
             print(f"{name} owes {ap}")
 
     print("--------------Breakdown--------------")
 
-    total_debt = sum(debt_collectors.values())
+    total_debt = sum(creditors.values())
     share_of_debt = {}
-    for name in debt_collectors.keys():
-        share_of_debt[name] = debt_collectors[name] / total_debt
+    for name in creditors.keys():
+        share_of_debt[name] = creditors[name] / total_debt
     
-    for name in indebted.keys():
+    for name in debtors.keys():
         for owed in share_of_debt.keys():
-            amount_owed = np.round(indebted[name] * share_of_debt[owed], 2)
+            amount_owed = np.round(debtors[name] * share_of_debt[owed], 2)
             print(f"{name} owes {owed} {amount_owed}")
